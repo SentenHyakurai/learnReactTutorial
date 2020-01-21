e65cf40fe69909bfe7bc4280e690adb6689ea540
DDLOTT <dmdlott@gmail.com>
2020-01-20 20:21:57 -0600

    working static state display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title, Card, Column} from 'rbx';
+import { Button, Container, Title, Card, Column, Level, Table} from 'rbx';
+import Sidebar from "react-sidebar";
 
 const Filler = ({}) => (
   <div>test</div>
 );
 
+
+
 const App = () => {
   const [data, setData] = useState({});
+  const [cartOpen, setCartOpen] = useState(true);
+  const [cartContents, setCartContents] = useState([{"title": "Tso 3D Short Sleeve T-Shirt A", "price": 10.9, "count": 2, "size": "XL"}, {"title": "Tso 3D Short Sleeve T-Shirt A", "price": 10.9, "count": 2, "size": "M"}]);
   const products = Object.values(data);
   useEffect(() => {
-    const fetchProducts = async () => {
+    const fetchProducts = async () => {s
       const response = await fetch('./data/products.json');
       const json = await response.json();
       setData(json);
@@ -21,9 +26,54 @@ const App = () => {
   console.log(products.length);
 
   return (
-    <CardGrid products={products} />
+    <div>
+      <HeadBar />
+      <Sidebar sidebar={<CartContent itemsArr={cartContents} />} open={cartOpen}  styles={{ sidebar: { background: "white" } }}/>
+      <CardGrid products={products} />
+    </div>
   );
 };
+
+function totalCost(itemsArr) {
+  var totalcost = 0;
+  itemsArr.forEach(obj => totalcost += obj.price * obj.count);
+  return totalcost.toString();
+}
+
+const CartContent = ({itemsArr}) => (
+    <div>
+      <Title> Shopping Cart </Title>
+      <Table>
+        <Table.Body>
+          <React.Fragment>
+           { itemsArr.map(obj => <CartItem item={ obj } /> ) }
+          </React.Fragment>
+            <Table.Row>
+              <Table.Cell>
+                {'total price = $' + totalCost(itemsArr)}
+              </Table.Cell>
+            </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
+);
+
+const CartItem = ({item}) => (
+  <Table.Row>
+    <Table.Cell>
+      {item.title + ' ' + item.size + ' x' + item.count + ': $' + item.price*item.count}
+    </Table.Cell>
+  </Table.Row>
+);
+
+const HeadBar = ({}) => (
+
+  <Level>
+    <Level.Item>
+      <Button>Shopping Cart</Button>
+    </Level.Item>
+  </Level>
+);
 
 const CardGrid = ({ products }) => ( 
   <Column.Group>
